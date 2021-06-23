fd477d7f863e9b31606b6909c15a0680427f10a4
mahidharvarma9 <64746169+mahidharvarma9@users.noreply.github.com>
2021-06-23 21:34:40 +0530

    Update admin.sh

--- admin.sh
+++ admin.sh
@@ -135,3 +135,4 @@ echo "----------------git clone completed-------------------"
 #curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 #. ~/.nvm/nvm.sh
 #nvm install node
+
