2283a4883ea793ef7b564811970e069c30945ad0
mahidharvarma9 <64746169+mahidharvarma9@users.noreply.github.com>
2021-06-30 12:42:12 +0530

    updated with node_exporter

--- admin.sh
+++ admin.sh
@@ -107,6 +107,23 @@ echo "---------------- Built admin-------------------"
 
 # echo "---------------- Built main-------------------"
 
+
+#installing node_exporter
+cd /tmp
+curl -LO https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+tar -xvf node_exporter-0.18.1.linux-amd64.tar.gz
+mv node_exporter-0.18.1.linux-amd64/node_exporter /usr/local/bin/
+useradd -rs /bin/false node_exporter
+#nano /etc/systemd/system/node_exporter.service
+cp ~/microservices/node_exporter.service ~/tmp/etc/systemd/system/node_exporter.service
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl status node_exporter
+sudo systemctl enable node_exporter
+#http://<server-IP>:9100/metrics
+
+echo "---------------- Installed node_exporter-------------------"
+
 ## migration for admin
 #docker-compose exec backend sh
 
