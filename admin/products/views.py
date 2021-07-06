51f94c543a6e714ff09c0f03257429232d490aea
mahidharvarma9 <64746169+mahidharvarma9@users.noreply.github.com>
2021-07-06 12:18:26 +0530

    delete function

--- admin/products/views.py
+++ admin/products/views.py
@@ -1,76 +1,56 @@
-from django.shortcuts import render
 from rest_framework import viewsets, status
-from rest_framework import serializers
-from rest_framework.serializers import Serializer
-from .models import Product, User
-from .serializers import ProductSerializer, UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-import random
-from .producer import publish
 
-# Create your views here.
+from .models import Product, User
+from .producer import publish
+from .serializers import ProductSerializer
+import random
+
 
 class ProductViewSet(viewsets.ViewSet):
-    def list(self, request): # /api/products
+    def list(self, request):
         products = Product.objects.all()
-        serializer = ProductSerializer(products, many = True)
-        # publish()
+        serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-    def create(self, request): # /api/products
+    def create(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        publish('product_created',serializer.data)
+        publish('product_created', serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    def retrieve(self, request, pk=None): # /api/products/<str:id>
+    def retrieve(self, request, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
-    def update(self, request, pk=None): # /api/products/<str:id>
+    def update(self, request, pk=None):
         product = Product.objects.get(id=pk)
         serializer = ProductSerializer(instance=product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        publish('product_updated',serializer.data)
+        publish('product_updated', serializer.data)
         return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
-
-    def destroy(self, request, pk=None): # /api/products/<str:id>
+    def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         product.delete()
-        publish('product_deleted',pk)
+        publish('product_deleted', pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
+        
+    def destroy_all(self, request, pk=None):
+    	products = Product.objects.all().delete()
+    	return Response(status=status.HTTP_204_NO_CONTENT)
+    	
+    		
 
-# class UserAPIView(APIView):
-#     def get(self, _):
-#         users = User.objects.all()
-#         user = random.choice(users)
-#         return Response({
-#             'id':user.id
-#         })
 
-#     def create_user(self, request): # /api/products
-#         serializer = UserSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save()
-#         publish('user_created',serializer.data)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-class UserAPIView(viewsets.ViewSet):
-    def get_user(self, request):
+class UserAPIView(APIView):
+    def get(self, _):
         users = User.objects.all()
         user = random.choice(users)
         return Response({
-            'id':user.id
+            'id': user.id
         })
-
-    def create_user(self, request): # /api/products
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        # publish('user_created',serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
