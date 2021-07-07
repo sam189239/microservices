3781f2c3c2b981132d82ac3f0b0112a093d9317e
mahidharvarma9 <64746169+mahidharvarma9@users.noreply.github.com>
2021-07-07 16:00:24 +0530

    zipkins

--- admin/products/views.py
+++ admin/products/views.py
@@ -7,19 +7,44 @@ from .producer import publish
 from .serializers import ProductSerializer
 import random
 
+import os
+from datetime import datetime
+import time
+import sys
+
+from opencensus.trace.tracer import Tracer
+from opencensus.trace import time_event as time_event_module
+from opencensus.ext.zipkin.trace_exporter import ZipkinExporter
+from opencensus.trace.samplers import AlwaysOnSampler
+
+# 1a. Setup the exporter
+ze = ZipkinExporter(service_name="test_api-tracing",
+                                host_name='zipkins',
+                                port=9411,
+                                endpoint='/api/v2/spans')
+# 1b. Set the tracer to use the exporter
+# 2. Configure 100% sample rate, otherwise, few traces will be sampled.
+# 3. Get the global singleton Tracer object
+tracer = Tracer(exporter=ze, sampler=AlwaysOnSampler())
+
+
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+            with tracer.span(name="get_products") as span:
+                    with tracer.span(name="get_products_chils") as span:
+                        print("getting products")
+                        products = Product.objects.all()
+                        serializer = ProductSerializer(products, many=True)
+                        return Response(serializer.data)
 
     def create(self, request):
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        publish('product_created', serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+            with tracer.span(name="create_product") as span:
+                serializer = ProductSerializer(data=request.data)
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                publish('product_created', serializer.data)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
         product = Product.objects.get(id=pk)
@@ -41,10 +66,10 @@ class ProductViewSet(viewsets.ViewSet):
         return Response(status=status.HTTP_204_NO_CONTENT)
         
     def destroy_all(self, request, pk=None):
-    	products = Product.objects.all().delete()
-    	return Response(status=status.HTTP_204_NO_CONTENT)
-    	
-    		
+            products = Product.objects.all().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+            
+                    
 
 
 class UserAPIView(APIView):
