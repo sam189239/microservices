509900ab1351f35519ac7670e739e600c0199f90
mahidharvarma9 <64746169+mahidharvarma9@users.noreply.github.com>
2021-07-09 18:35:08 +0530

    updated

--- main/producer.py
+++ main/producer.py
@@ -1,12 +1,11 @@
 import pika, json
 
-params = pika.URLParameters('amqps://oenwuqee:91pzsvj2f941LHWu7ZY0-L9D5ojQ_v1K@snake.rmq2.cloudamqp.com/oenwuqee')
 
-connection = pika.BlockingConnection(params)
-
-channel = connection.channel()
 
 
 def publish(method, body):
+    params = pika.URLParameters('amqps://oenwuqee:91pzsvj2f941LHWu7ZY0-L9D5ojQ_v1K@snake.rmq2.cloudamqp.com/oenwuqee?heartbeat=65535')
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
     properties = pika.BasicProperties(method)
     channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
