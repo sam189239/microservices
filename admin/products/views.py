6b4b8eb511c979d220a94decd77f6b23b499cf12
DHRUV PATEL <pateldhruv1672@gmail.com>
2021-07-15 15:21:19 +0530

    Update views.py

--- admin/products/views.py
+++ admin/products/views.py
@@ -17,26 +17,37 @@ from opencensus.trace import time_event as time_event_module
 from opencensus.ext.zipkin.trace_exporter import ZipkinExporter
 from opencensus.trace.samplers import AlwaysOnSampler
 
+from opencensus.trace import config_integration
+
+INTEGRATIONS = ['mysql', 'sqlalchemy', 'requests','pymysql']
+
+config_integration.trace_integrations(INTEGRATIONS)
+
 # 1a. Setup the exporter
 ze = ZipkinExporter(service_name="test_api-tracing",
                                 host_name='zipkins',
                                 port=9411,
                                 endpoint='/api/v2/spans')
+
+INTEGRATIONS = ['mysql', 'sqlalchemy', 'requests','pymysql']
+
+config_integration.trace_integrations(INTEGRATIONS)
 # 1b. Set the tracer to use the exporter
 # 2. Configure 100% sample rate, otherwise, few traces will be sampled.
 # 3. Get the global singleton Tracer object
-tracer = Tracer(exporter=ze, sampler=AlwaysOnSampler())
+#tracer = Tracer(exporter=ze, sampler=AlwaysOnSampler())
 
 
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-            with tracer.span(name="get_products") as span:
-                    with tracer.span(name="get_products_chils") as span:
-                        print("getting products")
-                        products = Product.objects.all()
-                        serializer = ProductSerializer(products, many=True)
-                        return Response(serializer.data)
+        #tracer = Tracer(exporter=ze, sampler=AlwaysOnSampler())
+        with tracer.span(name="get_products") as span:
+            with tracer.span(name="get_products_chils") as span:
+                print("getting products")
+                products = Product.objects.all()
+                serializer = ProductSerializer(products, many=True)
+                return Response(serializer.data)
 
     def create(self, request):
             with tracer.span(name="create_product") as span:
