984b30c8fd5b3cc7af7549c8c167de5201be0b70
sam189239 <48877205+sam189239@users.noreply.github.com>
2021-07-15 13:04:51 +0530

    Update urls.py

--- admin/admin/urls.py
+++ admin/admin/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('products.urls'))
+    path('api/', include('products.urls')),
+    path('', include('django_prometheus.urls')),
 ]
